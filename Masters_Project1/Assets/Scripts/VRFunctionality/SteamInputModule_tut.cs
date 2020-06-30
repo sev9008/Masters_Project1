aa46000aabab6320e40c25c19a64c700215d89c3
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-06-30 13:03:14 -0700

    fixed VR pointer Finished Part1 UI Interactivity
    Can itneract with buttons using Pointer
    
    Can interact with slider Using pointer

---
+++ Masters_Project1/Assets/Scripts/VRFunctionality/SteamInputModule_tut.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SteamInputModule_tut : VRInputModule
+{
+    public SteamVR_Input_Sources TargetSource;
+    public SteamVR_Action_Boolean ClickAction = null;
+
+    public override void Process()
+    {
+        base.Process();
+
+        if (ClickAction.GetStateDown(TargetSource))
+        {
+            Press();
+        }
+
+        if (ClickAction.GetStateUp(TargetSource))
+        {
+            Release();
+        }
+    }
+}
