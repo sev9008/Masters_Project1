810863a0399857b094209f3d43a44e61498b9a14
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-07-27 14:04:02 -0700

    Added 2d Graph Animation SelectSort

---
+++ Masters_Project1/Assets/Scripts/VRFunctionality/sorting_algorithms/AlgorithmAni/EnablePanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePanel : MonoBehaviour
+{
+    public GameObject[] arr;
+
+    public int currentlyselected;
+    void Start()
+    {
+        currentlyselected = -1;
+        updatepanels();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void updatepanels()
+    {
+        if (currentlyselected == -1)
+        {
+            foreach (GameObject panel in arr)
+            {
+                panel.SetActive(false);
+            }
+        }
+
+        if (currentlyselected != -1)
+        {
+            int i = 0;
+            foreach (GameObject panel in arr)
+            {
+                panel.SetActive(false);
+                i++;
+                if (i == currentlyselected)
+                {
+                    panel.SetActive(true);
+                }
+            }
+        }
+    }
+
+}
---
+++ Masters_Project1/Assets/Scripts/VRFunctionality/sorting_algorithms/AlgorithmAni/graphani/StartSelectiongraphani.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartSelectiongraphani : MonoBehaviour, IPointerDownHandler
+{
+    public SelectionAni m_selectionAni;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        StartCoroutine(m_selectionAni.SelectSort(m_selectionAni.arr));
+    }
+}
