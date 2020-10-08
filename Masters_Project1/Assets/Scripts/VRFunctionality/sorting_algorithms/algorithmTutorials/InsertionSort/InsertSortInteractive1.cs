bedac17472302c634ae90b354763bfce5e56f5f0
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-10-08 10:36:47 -0700

    insertion sort ready for debugging

--- Masters_Project1/Assets/Scripts/VRFunctionality/sorting_algorithms/algorithmTutorials/InsertionSort/InsertSortInteractive1.cs
+++ Masters_Project1/Assets/Scripts/VRFunctionality/sorting_algorithms/algorithmTutorials/InsertionSort/InsertSortInteractive1.cs
@@ -6,47 +6,44 @@ using UnityEngine.UI;
 
 public class InsertSortInteractive1 : MonoBehaviour
 {
-    //public List<MyStruct> arr;
     public List<GameObject> b;
     public List<GameObject> pos;
     public Material Donesort;
     public Material Nextsort;
+    public Material prevsort;
     public Material Unsorted;
 
     public bool sorted;
-
-    public int step;
+    public bool waitingforswap;
 
     public Text Step;
 
-    public int correctAnswers;
-    public int incorretAnswers;
-    public int numofGames;
+    public VRController_1 m_vRController_1;
+
+    public int currentSmallestIndex;
+    public int nextToSort;
+    public float dist1;
+    public float dist2;
+
+    public Coroutine co;
+    public Coroutine sho;
+
+    private int corretAnswers;
+    private int incorretAnswers;
+    private int numofGames;
     public Text corretAnswersText;
     public Text incorretAnswersText;
     public Text numofGamesText;
 
-    public VRController_1 m_vRController_1;
+    public int speed;
+    public int keypos;
 
-    public GameObject arrow;
+    private GameObject tempGo;
 
-    public int NextSmallesIndex;
-    public int IndexToSwap;
-    public int FinalIndexToSwap;
-    public float dist1;
-
-    private bool swapping;
-
+    public GameObject keyGo;
 
     private void Start()
     {
-        corretAnswersText.GetComponentInChildren<Text>().text = "0";
-        incorretAnswersText.GetComponentInChildren<Text>().text = "0";
-        numofGamesText.GetComponentInChildren<Text>().text = "0";
-        incorretAnswers = 0;
-        correctAnswers = 0;
-        numofGames = 0;
-        arrow.SetActive(false);
         Begin();
     }
 
@@ -56,67 +53,72 @@ public class InsertSortInteractive1 : MonoBehaviour
         {
             EnableTrigger();
             Step.text = "Congrats!  The array is now Sorted!" + "\nThis is Generally how Insertion Sort Operates." + "\nThe Algorithm iterates thorugh the array and shifts the smallest values to the front.";
-            IndexToSwap = 0; 
-            NextSmallesIndex = 9;
         }
-        else 
+        if (waitingforswap && currentSmallestIndex != 2000)
         {
-            dist1 = Vector3.Distance(b[IndexToSwap].transform.position, b[NextSmallesIndex].transform.position);
+            Step.text = "The Algorithm is ready to swap values.  Perform the swap.";
+            dist1 = Vector3.Distance(b[nextToSort].transform.position, b[currentSmallestIndex].transform.position);
             if (dist1 < .04)
             {
                 Debug.Log("Swap");
-                correctAnswers++;
-
                 m_vRController_1.downR = false;
                 m_vRController_1.downL = false;
-                string tempstringvalue = b[NextSmallesIndex].GetComponentInChildren<Text>().text;
-                b[NextSmallesIndex].GetComponentInChildren<Text>().text = b[IndexToSwap].GetComponentInChildren<Text>().text;
-                b[IndexToSwap].GetComponentInChildren<Text>().text = tempstringvalue;
+                Step.text = "The block you attempted to swap was correct.";
+                corretAnswers += 1;
+                corretAnswersText.GetComponent<Text>().text = corretAnswers.ToString();
+                b[nextToSort].GetComponentInChildren<Text>().text = b[currentSmallestIndex].GetComponentInChildren<Text>().text;
+                waitingforswap = false;
                 updatePos();
-
-                if (IndexToSwap == FinalIndexToSwap)
-                {
-                    checksort();
-                }
-                else
-                {
-                    IndexToSwap--;
-                    NextSmallesIndex--;
-                    EnableTrigger();
-                }
+                return;
             }
-            for (int i = 0; i < NextSmallesIndex; i++)
+            else
+            {//check for incorret answer
+
+            }
+        }        
+        else if (waitingforswap && currentSmallestIndex == 2000)
+        {
+            Step.text = "The Algorithm is ready to swap values.  Perform the swap.";
+            dist1 = Vector3.Distance(b[nextToSort].transform.position, keyGo.transform.position);
+            if (dist1 < .04)
             {
-                dist1 = Vector3.Distance(b[i].transform.position, b[NextSmallesIndex].transform.position);
-                if (dist1 < .04 && i != NextSmallesIndex)
-                {
-                    m_vRController_1.downR = false;
-                    m_vRController_1.downL = false;
-                    incorretAnswers++;
-                    updatePos();
-                }
+                Debug.Log("Swap");
+                m_vRController_1.downR = false;
+                m_vRController_1.downL = false;
+                Step.text = "The block you attempted to swap was correct.";
+                corretAnswers += 1;
+                corretAnswersText.GetComponent<Text>().text = corretAnswers.ToString();
+                b[nextToSort].GetComponentInChildren<Text>().text = b[currentSmallestIndex].GetComponentInChildren<Text>().text;
+                waitingforswap = false;
+                updatePos();
+                return;
+            }
+            else
+            {//check for incorret answer
+
             }
         }
-        corretAnswersText.text = correctAnswers.ToString();
-        incorretAnswersText.text = incorretAnswers.ToString();
-        numofGamesText.text = numofGames.ToString();
     }
 
     public void Begin()
     {
-        numofGames += 1;
-        //numofGamesText.GetComponent<Text>().text = numofGames.ToString();
+        try
+        {
+            StopCoroutine(co);
+            StopCoroutine(sho);
+            updatePos();
+        }
+        catch { }
         Step.text = "Welcome!  This interactive minigame is designed to teach you how to perform Insertion Sort." + "\nIf the block is Green it is Sorted and can not be interacted with." + "\nIf a block is White it must be shifted to the elft until it is in its sorted position.";
         for (int i = 0; i < 9; i++)
         {
             int n = UnityEngine.Random.Range(1, 99);
             Text t = b[i].GetComponentInChildren<Text>();
             t.text = n.ToString();
-            b[i].GetComponent<MoveInteractionBLock>().PairedPos = pos[i];
         }
         updatePos();
         sorted = false;
-        checksort();
+        co = StartCoroutine(insertionSort());
     }
 
     public void updatePos()
@@ -126,84 +128,89 @@ public class InsertSortInteractive1 : MonoBehaviour
             b[i].GetComponent<RectTransform>().anchoredPosition = pos[i].GetComponent<RectTransform>().anchoredPosition;
             b[i].transform.position = pos[i].transform.position;
             b[i].transform.rotation = pos[i].transform.rotation;
+            keyGo.GetComponent<RectTransform>().anchoredPosition = new Vector2(b[keypos].GetComponent<RectTransform>().anchoredPosition.x, b[keypos].GetComponent<RectTransform>().anchoredPosition.y - 50);
+
         }
     }
-    public void checksort()
+    public IEnumerator insertionSort()
     {
-        for (int i = 0; i < b.Count-1; i++)
+        keyGo.SetActive(true);
+
+        int i, key, j;
+        for (i = 1; i < b.Count; i++)
         {
-            int j = i + 1;
-            Text ti = b[i].GetComponentInChildren<Text>();
-            Text tj = b[j].GetComponentInChildren<Text>();
-            int ni = Convert.ToInt32(ti.text);
-            int mi = Convert.ToInt32(tj.text);
-            if (ni > mi)
+            key = int.Parse(b[i].GetComponentInChildren<Text>().text);
+            b[i].GetComponentInChildren<MeshRenderer>().material = Donesort;
+            keyGo.GetComponentInChildren<Text>().text = key.ToString();
+            keypos = i;
+            keyGo.GetComponent<RectTransform>().anchoredPosition = new Vector2(b[i].GetComponent<RectTransform>().anchoredPosition.x, b[i].GetComponent<RectTransform>().anchoredPosition.y - 50);
+
+            j = i - 1;
+
+            int tempval = int.Parse(b[j].GetComponentInChildren<Text>().text);
+            while (j >= 0 && tempval > key)
             {
-                NextSmallesIndex = j;
-                FindSwap();
-                EnableTrigger();
-                sorted = false;
-                return;
+                b[j].GetComponentInChildren<MeshRenderer>().material = Nextsort;
+                b[j + 1].GetComponentInChildren<MeshRenderer>().material = prevsort;
+                updatePos();
+                nextToSort = j + 1;
+                currentSmallestIndex = j;
+                waitingforswap = true;
+                while (waitingforswap)
+                {
+                    yield return null;
+                }
+                b[j + 1].GetComponentInChildren<Text>().text = b[j].GetComponentInChildren<Text>().text;
+                j = j - 1;
+
+                if (j >= 0)//use this otherwise tmepval will throw an error
+                {
+                    tempval = int.Parse(b[j].GetComponentInChildren<Text>().text);
+                    Debug.Log(tempval);
+                }
+                yield return new WaitForSeconds(speed);
+            }
+            nextToSort = j + 1;
+            currentSmallestIndex = 2000;
+            waitingforswap = true;
+            while (waitingforswap)
+            {
+                yield return null;
+            }
+            b[j + 1].GetComponentInChildren<Text>().text = key.ToString();
+            yield return new WaitForSeconds(speed);
+            for (int p = 0; p < b.Count; p++)
+            {
+                b[p].GetComponentInChildren<MeshRenderer>().material = Unsorted;
             }
         }
+        keyGo.SetActive(false);
+        updatePos();
         sorted = true;
+        yield return new WaitForSeconds(speed);
     }
 
-    public void FindSwap()
-    {
-        int tempindex = 0;
-        for (int i = NextSmallesIndex-1; i >= 0; i--)
-        {
-            Text ti = b[i].GetComponentInChildren<Text>();
-            Text tj = b[NextSmallesIndex].GetComponentInChildren<Text>();
-            int ni = Convert.ToInt32(ti.text);
-            int mi = Convert.ToInt32(tj.text);
-
-            if (ni > mi)
-            {
-                tempindex = i;
-            }
-            if (ni < mi)
-            {
-                break;
-            }
-        }
-        IndexToSwap = NextSmallesIndex-1;
-        FinalIndexToSwap = tempindex;
-    }
     public void EnableTrigger()
     {
         for (int i = 0; i < b.Count; i++)
         {
             if (sorted)
             {
-                b[i].GetComponent<BoxCollider>().enabled = false;
-                b[i].GetComponent<MoveInteractionBLock>().enabled = true;
-                pos[i].GetComponent<BoxCollider>().enabled = false;
                 b[i].GetComponentInChildren<MeshRenderer>().material = Donesort;
             }
-            else 
+            else
             {
-                if (i == NextSmallesIndex)
+                if (i == currentSmallestIndex)
                 {
-                    b[i].GetComponent<MoveInteractionBLock>().enabled = true;
                     b[i].GetComponentInChildren<MeshRenderer>().material = Nextsort;
-                    b[i].GetComponent<BoxCollider>().enabled = true;
-                    pos[i].GetComponent<BoxCollider>().enabled = false;
                 }
-                else if (i < NextSmallesIndex)
+                else if (i < nextToSort)
                 {
-                    b[i].GetComponent<MoveInteractionBLock>().enabled = false;
                     b[i].GetComponentInChildren<MeshRenderer>().material = Donesort;
-                    b[i].GetComponent<BoxCollider>().enabled = false;
-                    pos[i].GetComponent<BoxCollider>().enabled = true;
                 }
                 else
                 {
-                    b[i].GetComponent<MoveInteractionBLock>().enabled = true;
                     b[i].GetComponentInChildren<MeshRenderer>().material = Unsorted;
-                    b[i].GetComponent<BoxCollider>().enabled = false;
-                    pos[i].GetComponent<BoxCollider>().enabled = false;
                 }
             }
         }
