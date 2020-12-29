5e8661fb6ae8a451c6fd7384a6e7708b0a4af45a
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-12-29 13:06:25 -0800

    Finished Polymorphism

---
+++ Masters_Project1/Assets/Scripts/VRFunctionality/Paradigm_Algorithm/ObjectOriented/Inheritance/InheritancePause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InheritancePause : MonoBehaviour
+{
+    public InheritanceController inheritanceController;
+    public Text txt;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (inheritanceController.manual)
+        {
+            inheritanceController.manual = true;
+            txt.text = "Resume";
+        }
+
+        else if (!inheritanceController.manual)
+        {
+            inheritanceController.manual = false;
+            txt.text = "Pause";
+        }
+    }
+}
