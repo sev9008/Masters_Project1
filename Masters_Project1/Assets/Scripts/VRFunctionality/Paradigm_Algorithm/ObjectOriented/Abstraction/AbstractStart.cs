42931669ab101e62fca86e128cc508416da140ee
Benjamin Alexander <005886968@coyote.csusb.edu>
2021-01-01 11:42:55 -0800

    Finished Abstraction

---
+++ Masters_Project1/Assets/Scripts/VRFunctionality/Paradigm_Algorithm/ObjectOriented/Abstraction/AbstractStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AbstractStart : MonoBehaviour
+{
+    public AbstractController abstractController;
+    public Text txt;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (abstractController.begin)
+        {
+            abstractController.begin = true;
+            txt.text = "Start";
+        }
+
+        else if (!abstractController.begin)
+        {
+            abstractController.begin = false;
+            txt.text = "Stop";
+        }
+    }
+}
