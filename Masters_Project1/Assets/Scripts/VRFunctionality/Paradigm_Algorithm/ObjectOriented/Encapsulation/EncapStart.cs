7f0b147b38dd40b4b53970202aed6182c1f4a990
Benjamin Alexander <005886968@coyote.csusb.edu>
2021-01-03 10:58:41 -0800

    Finished Encapsulation

---
+++ Masters_Project1/Assets/Scripts/VRFunctionality/Paradigm_Algorithm/ObjectOriented/Encapsulation/EncapStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EncapStart : MonoBehaviour
+{
+    public EncapController encapController;
+    public Text txt;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (encapController.begin)
+        {
+            encapController.begin = true;
+            txt.text = "Start";
+        }
+
+        else if (!encapController.begin)
+        {
+            encapController.begin = false;
+            txt.text = "Stop";
+        }
+    }
+}
