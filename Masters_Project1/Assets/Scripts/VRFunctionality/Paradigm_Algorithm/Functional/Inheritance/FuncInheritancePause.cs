bd80060e0ddf44c9c1c176f2b2871c6ae5d4501f
Benjamin Alexander <005886968@coyote.csusb.edu>
2021-03-18 15:52:53 -0700

    Added Functional Paradigm Tutorials

---
+++ Masters_Project1/Assets/Scripts/VRFunctionality/Paradigm_Algorithm/Functional/Inheritance/FuncInheritancePause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FuncInheritancePause : MonoBehaviour
+{
+    public FuncInheritanceController abstractController;
+    public Text txt;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (abstractController.manual)
+        {
+            abstractController.manual = true;
+            txt.text = "Resume";
+        }
+
+        else if (!abstractController.manual)
+        {
+            abstractController.manual = false;
+            txt.text = "Pause";
+        }
+    }
+}
